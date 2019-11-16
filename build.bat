5a01f46524598dcb91efcda9637c2ca9d2800e94
Andrei Punko <andd3dfx@gmail.com>
2019-11-16 21:44:19 +0300

    Fix build script to put executable files into %GOPATH%\pkg folder

--- build.bat
+++ build.bat
@@ -1,12 +1,14 @@
-rem Remove ./out directory
-rmdir /s/Q out
+
+rem Remove already created files
+del %GOPATH%\bin\linux-amd64\web-cache
+del %GOPATH%\bin\windows-amd64\web-cache.exe
 
 rem Build executable file for Linux OS
 set GOOS=linux
 set GOARCH=amd64
-go build -o ./out/linux-amd64/web-cache ./main/web_server.go
+go build -o %GOPATH%\bin\linux-amd64\web-cache ./main/web_server.go
 
 rem Build executable file for Windows OS
 set GOOS=windows
 set GOARCH=amd64
-go build -o ./out/windows-amd64/web-cache.exe ./main/web_server.go
+go build -o %GOPATH%\bin\windows-amd64\web-cache.exe ./main/web_server.go
